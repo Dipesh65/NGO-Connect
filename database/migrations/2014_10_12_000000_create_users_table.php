ca89201d55c86f1f967b5a08dc9843435a308b44
Dipesh65 <97532545+Dipesh65@users.noreply.github.com>
2025-08-28 16:10:32 +0545

    Added Laravel Breeze for Authentication

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,10 +13,14 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('name')->nullable();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('phone')->nullable();
+            $table->string('address')->nullable();
+            $table->string('picture_path_name')->nullable();
+            $table->enum('role',[0,1,2]);
             $table->rememberToken();
             $table->timestamps();
         });
